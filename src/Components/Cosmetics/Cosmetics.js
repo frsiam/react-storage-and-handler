786e6dd0dea2bfbd6a5ca729691af8eebb0ac827
frsiam <siam.frs@gmail.com>
2022-03-22 23:31:18 +0600

    calculate the total price using reduce on a array of object

--- src/Components/Cosmetics/Cosmetics.js
+++ src/Components/Cosmetics/Cosmetics.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { getTotal } from '../../Utilities/calculate';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
@@ -9,8 +10,11 @@ const Cosmetics = () => {
         .then(res => res.json())
         .then(data => setCosmetics(data))
     },[])
+    const total = getTotal(cosmetics)
     return (
         <div>
+            <h1>Hello ! Welcome to my cosmetic store</h1>
+            <p>Money Needed: {total}</p>
             {
                 cosmetics.map(cosmetic => <Cosmetic name={cosmetic.name} price={cosmetic.price} key={cosmetic._id} id={cosmetic._id}></Cosmetic>)
             }
--- src/Utilities/calculate.js
+++ src/Utilities/calculate.js
@@ -6,5 +6,27 @@ const add = (first , second) =>{
 const multiply = (first,second) =>{
     return first * second;
 }
+// const numbers = [23,20,45,96,38]
+// const sumReducer = (previous,current) => previous + current;
+// const total = numbers.reduce(sumReducer,0)
 
-export {add, multiply}
+// const items = [
+//     {id: 1, name: 'alta', price: 48},
+//     {id: 2, name: 'alta', price: 38},
+//     {id: 3, name: 'alta', price: 68},
+//     {id: 4, name: 'alta', price: 28}
+// ]
+// const itemSumReducer = (previous,current) => previous + current.price
+// const itemTotal = items.reduce(itemSumReducer,0)
+
+const getTotalPrice = products =>{
+    const reducer = (previous,current) => previous + current.price
+    const total = products.reduce(reducer,0)
+    return total
+}
+
+export {
+    add,
+    multiply,
+    getTotalPrice as getTotal
+}
